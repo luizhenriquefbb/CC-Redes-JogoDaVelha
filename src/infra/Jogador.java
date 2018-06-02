8e66b3b9a2a3736d24f0c84446562c4d839d696b
Luiz Henrique Freire Barros <luizhenriquefbb@gmail.com>
2018-06-02 17:26:51 -0300

    add Jogo e Jogador

---
+++ src/infra/Jogador.java
@@ -0,0 +1,18 @@
+package infra;
+
+public class Jogador {
+
+	String nome;
+
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
