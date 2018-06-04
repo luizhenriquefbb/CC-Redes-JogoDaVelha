1c5c2b92c392d17402bf741f0f4bef6d472d2f6c
Luiz Henrique Freire Barros <luizhenriquefbb@gmail.com>
2018-06-04 15:34:11 -0300

    initial commit

--- src/infra/Jogador.java
+++ src/infra/Jogador.java
@@ -2,11 +2,18 @@ package infra;
 
 public class Jogador {
 
-	String nome;
+	private String nome;
+	private String ipServidor;
+	private int idSala;
+	private Tabuleiro tabuleiro;
+	private boolean emJogo;
 
-	public Jogador(String nome) {
-		this.nome = nome;
-	}
+	void convidar;
+	void aceitar;
+	void mandaStatus; // 'estou online'
+	void mandaJogada;
+	void desistirPartida;
+
 
 	public String getNome() {
 		return nome;
@@ -15,4 +22,37 @@ public class Jogador {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getIpServidor() {
+		return ipServidor;
+	}
+
+	public void setIpServidor(String ipServidor) {
+		this.ipServidor = ipServidor;
+	}
+
+	public int getIdSala() {
+		return idSala;
+	}
+
+	public void setIdSala(int idSala) {
+		this.idSala = idSala;
+	}
+
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	public void setTabuleiro(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	public boolean isEmJogo() {
+		return emJogo;
+	}
+
+	public void setEmJogo(boolean emJogo) {
+		this.emJogo = emJogo;
+	}
+
 }
